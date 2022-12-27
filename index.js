52c75740ac92a765210042e2d8664704deb96076
isnx <mazur.maciek@yahoo.com>
2022-12-27 12:18:46 +0100

    27.12.2022
    added:
    - password copy on click
    - inputs for password type
    - minor css

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 console.log("start")
-
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
-
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"]
+let characters = []
 let headerOne = document.getElementById("header-one")
     headerOne.textContent = "Generate a"
 let headerTwo = document.getElementById("header-two")
@@ -15,7 +16,44 @@ let btn = document.getElementById("generate-password")
     btn.textContent = "Generate password"
 let passOne = document.getElementById("password-one")
 let passTwo = document.getElementById("password-two")
+let copyConfirmation = document.getElementById("copy-confirmation")
+const myTimeout = setTimeout(classReset, 5000);
+let letInput = document.getElementById("let-input")
+let numInput = document.getElementById("num-input")
+let symInput = document.getElementById("sym-input")
 
+function classReset(){
+    copyConfirmation.classList.add("display-off");
+    copyConfirmation.classList.remove("display-on");
+}
+
+function renderPassword(){
+    if (letInput.checked && numInput.checked && symInput.checked){
+    characters = letters.concat(numbers, symbols)
+    generatePassword()
+    } else if(letInput.checked && numInput.checked && symInput.checked === false ){
+        characters = letters.concat(numbers)
+        generatePassword()
+    } else if(letInput.checked && numInput.checked === false && symInput.checked){
+        characters = letters.concat(symbols)
+        generatePassword()
+    } else if(letInput.checked && numInput.checked === false && symInput.checked === false){
+        characters = letters
+        generatePassword()
+    } else if(letInput.checked === false && numInput.checked && symInput.checked){
+        characters = numbers.concat(symbols)
+        generatePassword()
+    } else if(letInput.checked === false && numInput.checked && symInput.checked === false){
+        characters = numbers
+        generatePassword()
+    } else if(letInput.checked === false && numInput.checked === false && symInput.checked){
+        characters = symbols
+        generatePassword()
+    } else{
+        alert("check at least one box")
+    }
+}
+ 
 function generatePassword(){
     passwordOne = ""
     passwordTwo = ""
@@ -29,6 +67,26 @@ function generatePassword(){
     passTwo.textContent = passwordTwo
 }
 
-btn.addEventListener("click", generatePassword)
 
+btn.addEventListener("click", renderPassword)
+
+function copyToClipboard(x){
+    navigator.clipboard.writeText(x);
+    copyConfirmation.classList.remove("display-off");
+    copyConfirmation.classList.add("display-on");
+    copyConfirmation.textContent = "Password Saved"
+    setTimeout(classReset, 1500)
+}
+function copyTextPwdOne(){
+    const text = document.getElementById('password-one').textContent
+    copyToClipboard(text)
+  }
+
+  function copyTextPwdTwo(){
+    const text = document.getElementById('password-two').textContent
+    copyToClipboard(text)
+  }
+
+  passOne.addEventListener("click", copyTextPwdOne)
+  passTwo.addEventListener("click", copyTextPwdTwo)
 
